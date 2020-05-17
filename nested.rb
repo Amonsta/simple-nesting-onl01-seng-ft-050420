f7f69b0dcc1245f1c5dc619562681d48667d631b
Amana Ammishaddai <amana.ammi@gmail.com>
2020-05-17 14:14:54 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -85,7 +85,7 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"] }
+programmer_hash[:yukihiro_matsumoto] = {}
      
     ## How can I add to the top of the hash??
           
